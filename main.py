0c3308f5fe50c977a116adce26a114e82d297f85
Arcade-Master-Team <arcade.master.team@gmail.com>
2026-06-25 21:22:51 -0400

    Added TTS to main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from gtts import gTTS
+import os
+from mutagen.mp3 import MP3
+import time
 
+#Text to Speech___________
+
+language = 'es-us'
+
+def speak(text):
+    speech = gTTS(text= text, lang= language, slow= False)
+    speech.save("texto.mp3")
+    os.system("start texto.mp3")
+    audio = MP3("texto.mp3")
+    time.sleep(audio.info.length + 0.5)
